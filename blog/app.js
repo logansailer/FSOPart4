d8217e66bacc50f2b4cc3c683b56c14234292c41
Logan <logansailer0633@gamil.com>
2024-09-25 15:52:13 -0400

    added router connection to app.js

--- blog/app.js
+++ blog/app.js
@@ -1,41 +1,27 @@
 const config = require("./utils/config");
 const logger = require("./utils/logger");
+const blogsRouter = require("./controllers/blogs");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
+//connects to mongoDB server
 mongoose.connect(config.MONGODB_URI);
-
 logger.info("connecting to", config.MONGODB_URI);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-  .save()
-  .then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info("Server running on port", config.PORT);
