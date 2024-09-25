c652339e9aab633b5bfa39170924d41098d94faa
Logan <logansailer0633@gamil.com>
2024-09-25 16:14:55 -0400

    deleted repeat .listens

--- blog/app.js
+++ blog/app.js
@@ -23,8 +23,4 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 
-app.listen(config.PORT, () => {
-  logger.info("Server running on port", config.PORT);
-});
-
 module.exports = app;
