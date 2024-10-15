f6901ec399d8f72647685f5f58dda82eeb2e2772
Logan <logansailer0633@gamil.com>
2024-10-14 20:27:43 -0400

    bug fixes

--- part4/app.js
+++ part4/app.js
@@ -1,11 +1,12 @@
 const config = require("./utils/config");
 const express = require("express");
-require("express-async-errors");
 const app = express();
+require("express-async-errors");
 const cors = require("cors");
-const loginRouter = require("./controllers/login");
-const usersRouter = require("./controllers/users");
 const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
@@ -28,9 +29,14 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.use("/api/login", loginRouter);
-app.use("/api/users", usersRouter);
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
